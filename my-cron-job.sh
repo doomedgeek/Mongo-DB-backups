cd46cc1af1bdb59608fc9754b4ec647de2d1cdb2
m0d3v0p5 <doomedtechiegeek@gmail.com>
2023-02-26 11:43:59 +0330

    cron job added

---
+++ my-cron-job.sh
@@ -0,0 +1,6 @@
+0 20 * * * /path/to/backup_script.sh
+0 22 * * * /path/to/backup_script.sh
+0 2 * * * /path/to/backup_script.sh
+0 4 * * * /path/to/backup_script.sh
+0 0 */6 * * /path/to/delete_old_backups.sh
+
